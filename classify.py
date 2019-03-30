c3b7850b0db492ae059e6952b82d5a0e21dc2c5a
Jessica Hadiwijoyo <jessindo@gmail.com>
2019-03-30 16:36:05 -0700

    testing multiple object detection

--- classify.py
+++ classify.py
@@ -11,6 +11,28 @@ API_KEY = "AIzaSyB1g5SVzfrZqzNQc_7HzJEJFzrnFG_kJEo"
 # python predict.py YOUR_LOCAL_IMAGE_FILE juicy-apple ICN2381291597882699746
 client = vision.ImageAnnotatorClient()
 
+def localize_objects(path):
+    """Localize objects in the local image.
+
+    Args:
+    path: The path to the local file.
+    """
+    from google.cloud import vision
+    client = vision.ImageAnnotatorClient()
+
+    with open(path, 'rb') as image_file:
+        content = image_file.read()
+    image = vision.types.Image(content=content)
+
+    objects = client.object_localization(
+        image=image).localized_object_annotations
+
+    print('Number of objects found: {}'.format(len(objects)))
+    for object_ in objects:
+        print('\n{} (confidence: {})'.format(object_.name, object_.score))
+        print('Normalized bounding polygon vertices: ')
+        for vertex in object_.bounding_poly.normalized_vertices:
+            print(' - ({}, {})'.format(vertex.x, vertex.y))
 
 def is_rotten(content):
     project_id = "juicy-apple"
---
+++ test.py
@@ -0,0 +1,4 @@
+from classify import localize_objects 
+import io
+
+localize_objects("./appleOrange.jpg")
