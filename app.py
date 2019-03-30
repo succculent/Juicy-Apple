456b4d9e3f70f4d274c6b6d26333618cb8d01a59
Haoda Wang <harry@h313.info>
2019-03-30 02:25:20 -0700

    lmao

--- app.py
+++ app.py
@@ -3,8 +3,24 @@ API_KEY="AIzaSyB1g5SVzfrZqzNQc_7HzJEJFzrnFG_kJEo"
 from flask import Flask
 app = Flask(__name__)
 
+from google.cloud import vision
+from google.cloud.vision import types
+
+from google.cloud import automl_v1beta1
+from google.cloud.automl_v1beta1.proto import service_pb2
+
+client = vision.ImageAnnotatorClient()
+
+def open_image:
+	image = types.Image(content=content)
+
+	# Performs label detection on the image file
+	response = client.label_detection(image=image)
+	labels = response.label_annotations
+
 @app.route("/")
 def hello():
-    return "Hello World!"
+	return "Hello World!"
 
 app.run(host='0.0.0.0')
+
