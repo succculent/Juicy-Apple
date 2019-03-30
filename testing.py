0634e7093f6028141fc3a93c9b7601c0c3fbbe82
Ada Toydemir <toydemir@usc.edu>
2019-03-30 03:38:44 -0700

    Rb

--- testing.py
+++ testing.py
@@ -1,43 +1,43 @@
 from classify import is_rotten, is_rotten
 import io
 
-with io.open("apple_logo.png") as image_file:
+with io.open("apple_logo.png", 'rb') as image_file:
 	content = image_file.read()
 	print("apple_logo:")
 	print(is_rotten(content))
 	print()
 
-with io.open("apple_w_background.jpg") as image_file:
+with io.open("apple_w_background.jpg", 'rb') as image_file:
 	content = image_file.read()
 	print("apple_w_background:")
 	print(is_rotten(content))
 	print()
 
-with io.open("car.jpg") as image_file:
+with io.open("car.jpg", 'rb') as image_file:
 	content = image_file.read()
 	print("car:")
 	print(is_rotten(content))
 	print()
 
-with io.open("cartoon_apple.jpg") as image_file:
+with io.open("cartoon_apple.jpg", 'rb') as image_file:
 	content = image_file.read()
 	print("cartoon_apple:")
 	print(is_rotten(content))
 	print()
 
-with io.open("pear.jpg") as image_file:
+with io.open("pear.jpg", 'rb') as image_file:
 	content = image_file.read()
 	print("pear:")
 	print(is_rotten(content))
 	print()
 
-with io.open("rotten_apple_1.jpg") as image_file:
+with io.open("rotten_apple_1.jpg", 'rb') as image_file:
 	content = image_file.read()
 	print("rotten_apple_1:")
 	print(is_rotten(content))
 	print()
 
-with io.open("rotten_apple_2.jpg") as image_file:
+with io.open("rotten_apple_2.jpg", 'rb') as image_file:
 	content = image_file.read()
 	print("rotten_apple_2:")
 	print(is_rotten(content))
