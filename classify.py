e21367a8507114b6f0fc9a43febf19ecccf31cbb
Jessica Hadiwijoyo <jessindo@gmail.com>
2019-03-30 17:36:49 -0700

    testing cropping

--- classify.py
+++ classify.py
@@ -1,15 +1,79 @@
 import sys
 
+import argparse
+import io
+
 from google.cloud import automl_v1beta1
 from google.cloud.automl_v1beta1.proto import service_pb2
 from google.cloud.vision import types
 from google.cloud import vision
+from PIL import Image, ImageDraw
 
 API_KEY = "AIzaSyB1g5SVzfrZqzNQc_7HzJEJFzrnFG_kJEo"
 
 # To exectue the request:
 # python predict.py YOUR_LOCAL_IMAGE_FILE juicy-apple ICN2381291597882699746
 client = vision.ImageAnnotatorClient()
+
+def get_crop_hint(path):
+    """Detect crop hints on a single image and return the first result."""
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = types.Image(content=content)
+
+    crop_hints_params = types.CropHintsParams(aspect_ratios=[1.77])
+    image_context = types.ImageContext(crop_hints_params=crop_hints_params)
+
+    response = client.crop_hints(image=image, image_context=image_context)
+    hints = response.crop_hints_annotation.crop_hints
+
+    # Get bounds for the first crop hint using an aspect ratio of 1.77.
+    vertices = hints[0].bounding_poly.vertices
+
+    return vertices
+
+
+def draw_hint(image_file):
+    """Draw a border around the image using the hints in the vector list."""
+    vects = get_crop_hint(image_file)
+
+    im = Image.open(image_file)
+    draw = ImageDraw.Draw(im)
+    draw.polygon([
+        vects[0].x, vects[0].y,
+        vects[1].x, vects[1].y,
+        vects[2].x, vects[2].y,
+        vects[3].x, vects[3].y], None, 'red')
+    im.save('output-hint.jpg', 'JPEG')
+    print('Saved new image to output-crop.jpg')
+
+
+def crop_to_hint(image_file):
+    """Crop the image using the hints in the vector list."""
+    vects = get_crop_hint(image_file)
+
+    im = Image.open(image_file)
+    im2 = im.crop([vects[0].x, vects[0].y,
+                  vects[2].x - 1, vects[2].y - 1])
+    im2.save('output-crop.jpg', 'JPEG')
+    print('Saved new image to output-crop.jpg')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('image_file', help='The image you\'d like to crop.')
+    parser.add_argument('mode', help='Set to "crop" or "draw".')
+    args = parser.parse_args()
+
+    parser = argparse.ArgumentParser()
+
+    if args.mode == 'crop':
+        crop_to_hint(args.image_file)
+    elif args.mode == 'draw':
+        draw_hint(args.image_file)
 
 def localize_objects(path):
     """Localize objects in the local image.
@@ -48,10 +112,10 @@ def is_rotten(content):
         if(result.display_name == "rotten"):
             return True
     return False
-    # return request  # waits till request is returned
+    #return request  # waits till request is returned
 
 
-def is_fruit(content):
+def is_apple(content):
     image = types.Image(content=content)
 
     # Performs label detection on the image file
@@ -59,7 +123,7 @@ def is_fruit(content):
     labels = response.label_annotations
 
     for label in labels:
-        if label.description == "Apple" or label.description == "Banana" or label.description == "Orange":
+        if label.description == "Apple":
             return True
 
     return False
