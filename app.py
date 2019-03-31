c61bbf8e71977e4bb57d3ea740224e782e4f0b5c
Haoda Wang <harry@h313.info>
2019-03-30 20:32:35 -0700

    nice

--- app.py
+++ app.py
@@ -22,19 +22,20 @@ def classify():
     r = 0
     t = 0
     status['success'] = 1
+    status['fruit'] = {}
     images = crop_to_hint(request.files['file'].read())
     for i in range(len(images)):
         content = images[i].tobytes()
         if is_fruit(content):
             t += 1
-            status[i]['supported'] = 1
+            status['fruit'][i]['supported'] = 1
             if is_rotten(content):
-                status[i]['rotten'] = 1
+                status['fruit'][i]['rotten'] = 1
                 r += 1
             else:
-                status[i]['rotten'] = 0
+                status['fruit'][i]['rotten'] = 0
         else:
-            status[i]['supported'] = 0
+            status['fruit'][i]['supported'] = 0
 
         if r > 0:
             sendi(r, t)
